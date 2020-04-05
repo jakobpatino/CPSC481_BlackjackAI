0b77729ff8b61c1956b2d17ddbf63f946a190137
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-04-04 19:55:14 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -25,10 +25,61 @@ def card_values(card):
     value = card[-1]
     if value == '0' or value == 'J' or value == 'Q' or value == 'K':
         return 10
-    elif value >= '1' or value <= '9':   #EDIT: Shortened if statements values 1-9
-        return int(value)
+    elif value == '2':
+        return 2
+    elif value == '3':
+        return 3
+    elif value == '4':
+        return 4
+    elif value == '5':
+        return 5
+    elif value == '6':
+        return 6
+    elif value == '7':
+        return 7
+    elif value == '8':
+        return 8
+    elif value == '9':
+        return 9
     else:
         return 0
+
+def remove_from_deck_count(card, dealer):
+    value = card_values(card)
+    dealer.full_deck_total -= 1
+    if value == 0:
+        dealer.aces -= 1
+    elif value == 2:
+        dealer.twos -= 1
+    elif value == 3:
+        dealer.threes -= 1
+    elif value == 4:
+        dealer.fours -= 1
+    elif value == 5:
+        dealer.fives -= 1
+    elif value == 6:
+        dealer.sixes -= 1
+    elif value == 7:
+        dealer.sevens -= 1
+    elif value == 8:
+        dealer.eights -= 1
+    elif value == 9:
+        dealer.nines -= 1
+    elif value == 10:
+        dealer.tens -= 1
+
+def print_count(dealer):
+    print(dealer.full_deck_total)
+    print(dealer.tens)
+    print(dealer.nines)
+    print(dealer.eights)
+    print(dealer.sevens)
+    print(dealer.sixes)
+    print(dealer.fives)
+    print(dealer.fours)
+    print(dealer.threes)
+    print(dealer.twos)
+    print(dealer.aces)
 
 
 def calc_hand_total(hand):
@@ -44,7 +95,9 @@ def calc_hand_total(hand):
 
 
 def hit(hand, dealer):
+    remove_from_deck_count(dealer.full_deck[0], dealer)
     hand.append(dealer.full_deck.pop(0))
+    print_count(dealer)
 
 
 def check_naturals(player, dealer):
@@ -54,11 +107,15 @@ def check_naturals(player, dealer):
         player.natural = True
         if dealer.hole_down:
             dealer.hole_down = False
+            remove_from_deck_count(dealer.cards_in_hand[1], dealer)
+            print_count(dealer)
             show_hands(player, dealer)
     if dealer.hand_total == 21:
         dealer.natural = True
         if dealer.hole_down:
             dealer.hole_down = False
+            remove_from_deck_count(dealer.cards_in_hand[1], dealer)
+            print_count(dealer)
             show_hands(player, dealer)
 
 
@@ -110,7 +167,7 @@ def reset_round(player, dealer):
     player.winner = False
     player.natural = False
     player.hand_total = 0
-    player. hand_total_alt = 0
+    player.hand_total_alt = 0
 
     dealer.winner = False
     dealer.tie = False
@@ -123,9 +180,52 @@ def reset_round(player, dealer):
 
 
 def show_hands(player, dealer):
-    print("player:" + str(player.cards_in_hand), ": Hand =", player.hand_total)    # EDIT: temporary numeral value indicator for hand
+    print("player:" + str(player.cards_in_hand), ": Hand =",
+          player.hand_total)  # EDIT: temporary numeral value indicator for hand
     if dealer.hole_down:
         print("dealer: " + str(dealer.cards_in_hand[0]), ": Hand =", dealer.hand_total)
     else:
         print("dealer" + str(dealer.cards_in_hand), ": Hand =", dealer.hand_total)
+
+
+def calc_ten_prob(dealer):
+    probability = dealer.tens / dealer.full_deck_total
+    return probability
+
+def calc_nine_prob(dealer):
+    probability = dealer.nines / dealer.full_deck_total
+    return probability
+
+def calc_eight_prob(dealer):
+    probability = dealer.eights / dealer.full_deck_total
+    return probability
+
+def calc_seven_prob(dealer):
+    probability = dealer.sevens / dealer.full_deck_total
+    return probability
+
+def calc_six_prob(dealer):
+    probability = dealer.sixes / dealer.full_deck_total
+    return probability
+
+def calc_five_prob(dealer):
+    probability = dealer.fives / dealer.full_deck_total
+    return probability
+
+def calc_four_prob(dealer):
+    probability = dealer.fours / dealer.full_deck_total
+    return probability
+
+def calc_three_prob(dealer):
+    probability = dealer.threes / dealer.full_deck_total
+    return probability
+
+def calc_two_prob(dealer):
+    probability = dealer.twos / dealer.full_deck_total
+    return probability
+
+def calc_aces_prob(dealer):
+    probability = dealer.aces / dealer.full_deck_total
+    return probability
+
 
