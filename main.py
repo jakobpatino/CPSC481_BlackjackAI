fceba17e7a8e05573705865525dcdb3c16deffdc
LlsLopez <36282497+LlsLopez@users.noreply.github.com>
2020-04-21 02:33:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,24 +16,22 @@ def run_game():
         gf.declare_round(round_num)
         dealer.initial_deal(player)
         gf.show_hands(player, dealer)
-        gf.count_cards(player, dealer)
         dealer.maybe_natural(player)
         gf.check_naturals(player, dealer)
         if player.natural or dealer.natural:
             gf.natural_winner(player, dealer)
         if not player.winner and not dealer.winner and not dealer.tie:
             player.ai_turn(dealer)
-            gf.count_cards(player,dealer)
             if not dealer.winner:
                 dealer.dealer_move(player)
-                gf.count_cards(player, dealer)
                 if not player.winner:
                     gf.assign_winner(player, dealer)
         gf.declare_winner(player, dealer)
+        gf.count_cards(player, dealer)
         gf.reset_round(player, dealer)
         round_num += 1
         if round_num % 10 == 0:
-            dealer.reset_shuffle()
+            dealer.reset_shuffle(player)
         if round_num == 100:
             in_progress = False
         print("----------------------------------------------")
