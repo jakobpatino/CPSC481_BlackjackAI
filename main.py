fab7a8416e5f03f5d0a9b6bb0bb718118b92d009
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-04-30 11:40:59 -0700

    added comments

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import functions as gf
 from dealer import Dealer
 from player import Player
 
-
+#runs 1000 rounds of the game and prints the stats at the end to see how well the AI did (wins, losses, ties, money made)
+#first the dealer shuffles the deck and prints out the shuffled deck at the beginning
+#at the beginning of each round, it will display the round number and the initial hands of both the dealer and the player
+#will then check the naturals of both the player and the dealer and see if there is a winner early
+#if not then it is now the players turn; after the players turn, the dealer will then go until he hits 17 or busts
+#a winner is assigned and the round resets.
+#if the cards played gets above 225, which is about 4 decks, it will do a reset shuffle(make it back to 6 decks)
+#once it reaches 1000 rounds, it will display the stats for the run as mentioned before
 def run_game():
     dealer = Dealer()
     player = Player()
