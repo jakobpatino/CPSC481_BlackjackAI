fe07910b0a86328f0e5233f73e7c7ba97d783901
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-04-30 10:27:32 -0700

    add comments

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import functions as gf
 
-
+#initializes necessary parameters such as the hand total, the betting, etc.
 class Player:
 
     def __init__(self):
@@ -15,7 +15,7 @@ class Player:
         self.tie_count = 0
         self.dealer_win_count = 0
 
-        # 2-6 = +1     7-9 = +0      10 and Ace = -1
+        # 2-6 = +1     7-9 = +0      10, Face and Ace = -1
         self.bankroll = 10000  # starting amt of money
         self.betAmount = 0
         self.running_total = 0
@@ -23,6 +23,10 @@ class Player:
         self.card_total = 0  # total cards accounted for so far
         self.max_money = 10000
 
+    #main function for the AI functionalitily
+    #first it gets the bet ammount and the opponents probability of busting and the ai_probability of busting
+    #then it checks if the dealer is holding down; if so it assumes that the face down is a value of 10 bc of basic Blackjack strategy
+    #the AI will always assume that the dealer may get at least 17 so it puts that as the true assumption
     def ai_turn(self, dealer):
         stay = False
         self.betAmount = gf.determine_bet_amt(self)  # update how much is bet for the round
@@ -44,7 +48,11 @@ class Player:
             self.true_assumption = self.assumption
 
         print("----AI's Turn----")
-
+        
+        #if the AI has a hand between 17 and 21 then it will stay bc that is considered a good hand
+        #if assumption < 17, then if the probability of busting is lower then the oppenents then it will decide to hit/dd
+        #if decision was to DD then the AI stays and if the AI busted after hitting/dd then it will print bust and the dealer wins
+        #else if the assumption is over 17 and the hand total isnt between 17 and 21, then the AI will hit/dd
         while stay is False:
             if 17 <= self.hand_total <= 21 or 17 <= self.hand_total_alt <= 21:
                 stay = True
