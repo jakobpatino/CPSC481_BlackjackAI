1d76765d1e5ec9450e680a545c074f247e947b75
ryanwalton <31267577+ryanwalton@users.noreply.github.com>
2020-04-30 09:56:29 -0700

    added comments

--- dealer.py
+++ dealer.py
@@ -1,6 +1,6 @@
 import functions as gf
 
-
+#initializes the deck, all of the individual card counts, its hand total, if it needs to hole down, etc.
 class Dealer:
 
     def __init__(self):
@@ -11,8 +11,8 @@ class Dealer:
                      'DA', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'DJ', 'DQ', 'DK']
         self.full_deck = self.deck + self.deck + self.deck + self.deck + self.deck + self.deck
         self.cards_in_hand = []
-        self.hand_total = 0
-        self.hand_total_alt = 0
+        self.hand_total = 0             #hand total if there is an ace and you want a 11
+        self.hand_total_alt = 0         #hand total if there is an ace and you want a 1
         self.full_deck_total = 312
 
         self.tens = 96
@@ -31,11 +31,13 @@ class Dealer:
         self.natural = False
         self.tie = False
         self.hole_down = True
-
+    
+    #shuffles the entire full deck(6 decks shuffled together)
     def full_shuffle(self):
         for x in range(5):
             self.full_deck = gf.shuffle(self.full_deck)
 
+    #resets the deck and puts all values back to their original values before playing
     def reset_shuffle(self, player):
         self.full_deck = self.deck + self.deck + self.deck + self.deck + self.deck + self.deck
         self.full_shuffle()
@@ -54,6 +56,7 @@ class Dealer:
         player.true_total = 0
         player.card_total = 0
 
+    #dealer deals the first cards to themselves and the player
     def initial_deal(self, player):
         gf.remove_from_deck_count(self.full_deck[0], self, player)
         gf.remove_from_deck_count(self.full_deck[1], self, player)
@@ -63,10 +66,15 @@ class Dealer:
             player.cards_in_hand.append(self.full_deck.pop(0))
             self.cards_in_hand.append(self.full_deck.pop(0))
 
+    #resets the player and dealer's hands after a round is over
     def reset_hands(self, player):
         player.cards_in_hand = []
         self.cards_in_hand = []
 
+    #code for when it's the dealers turn
+    #dealer gets his cards and shows what he has; then goes into a while loop that ends once it decides what its hand is
+    #if its total is between 17 and 21 then it stays and while loop is exited, if its under 17 then it needs to hit
+    #will then check if its busted(goes over 21)
     def dealer_move(self, player):
         print("----Dealer Turn----")
         gf.calc_hand_total(self)
@@ -88,7 +96,8 @@ class Dealer:
         if gf.check_bust(self.true_hand_total):
             player.winner = True
             print("BUST")
-
+    
+    #if a card is a 10, face, or an ace, then the dealer will show both of his cards before the player gets to play
     def maybe_natural(self, player):
         card = self.cards_in_hand[0]
         num = card[-1]
