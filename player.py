7ab8abdcce1988f5170ffa8d84ce8dcd3b195b41
jakobpatino <32020852+jakobpatino@users.noreply.github.com>
2020-04-30 18:35:34 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,32 +1,32 @@
 import functions as gf
 
-#initializes necessary parameters such as the hand total, the betting, etc.
+
+# initializes necessary parameters such as the hand total, the betting, etc.
 class Player:
 
     def __init__(self):
         self.cards_in_hand = []
         self.winner = False
-        self.hand_total = 0
-        self.hand_total_alt = 0
+        self.hand_total = 0       # hand total if there is an ace and you want a 11
+        self.hand_total_alt = 0   # hand total if there is an ace and you want a 1
         self.natural = False
-        self.assumption = 0
-        self.true_assumption = 0
+        self.assumption = 0       # what we assume the dealer's current hand is
+        self.true_assumption = 0  # what we assume the dealer's final hand is
         self.win_count = 0
         self.tie_count = 0
         self.dealer_win_count = 0
 
-        # 2-6 = +1     7-9 = +0      10, Face and Ace = -1
-        self.bankroll = 10000  # starting amt of money
+        self.bankroll = 10000     # starting amt of money
         self.betAmount = 0
-        self.running_total = 0
-        self.true_total = 0
-        self.card_total = 0  # total cards accounted for so far
-        self.max_money = 10000
+        self.running_total = 0    # current total using card counting method
+        self.true_total = 0       # running total divided by amount of decks
+        self.card_total = 0       # total cards accounted for so far
 
-    #main function for the AI functionalitily
-    #first it gets the bet ammount and the opponents probability of busting and the ai_probability of busting
-    #then it checks if the dealer is holding down; if so it assumes that the face down is a value of 10 bc of basic Blackjack strategy
-    #the AI will always assume that the dealer may get at least 17 so it puts that as the true assumption
+    # main function for the AI functionality
+    # first it gets the bet amount and the opponents probability of busting and the ai_probability of busting
+    # then it checks if the dealer's hole is down;
+    # if so it assumes that the face down is a value of 10 bc of basic Blackjack strategy
+    # the AI will always assume that the dealer may get at least 17 so it puts that as the true assumption
     def ai_turn(self, dealer):
         stay = False
         self.betAmount = gf.determine_bet_amt(self)  # update how much is bet for the round
@@ -48,11 +48,13 @@ class Player:
             self.true_assumption = self.assumption
 
         print("----AI's Turn----")
-        
-        #if the AI has a hand between 17 and 21 then it will stay bc that is considered a good hand
-        #if assumption < 17, then if the probability of busting is lower then the oppenents then it will decide to hit/dd
-        #if decision was to DD then the AI stays and if the AI busted after hitting/dd then it will print bust and the dealer wins
-        #else if the assumption is over 17 and the hand total isnt between 17 and 21, then the AI will hit/dd
+
+        # if the AI has a hand between 17 and 21 then it will stay bc that is considered a good hand
+        # if assumption < 17, then if the probability of busting is lower than the opponent's,
+        # then it will decide to hit/dd, else it will stay
+        # if decision was to DD then the AI stays and if the AI busted after hitting/dd,
+        # then it will print bust and the dealer wins
+        # else if the assumption is over 17 and the hand total isnt between 17 and 21, then the AI will hit/dd
         while stay is False:
             if 17 <= self.hand_total <= 21 or 17 <= self.hand_total_alt <= 21:
                 stay = True
@@ -83,4 +85,3 @@ class Player:
                     dealer.winner = True
 
             gf.show_hands(self, dealer)
-
